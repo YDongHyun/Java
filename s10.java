d2f2637b560d5e81180aef0224470a7b88ab26fb
양동현 <80799025+YDongHyun@users.noreply.github.com>
2021-10-07 22:59:14 +0900

    Add files via upload

---
+++ s10.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class s10 {
+
+	public static void main(String[] args) {
+		// 조건연산자
+		int a=45;
+		int b=47;
+		
+		char ch;
+		ch=(a>b) ? 'T':'F';  // 조건식 ? 결과1 : 결과2;  (조건이 참이면 결과1, 아니면 결과2)
+		
+		System.out.println(ch);
+
+	}
+
+}
